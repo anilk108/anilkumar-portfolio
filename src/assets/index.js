d93b3b4e7e0b18bcbaa84eec86efe331efc48172
anilk108 <43748737+anilk108@users.noreply.github.com>
2024-01-27 22:52:52 -0600

    Fix resume import conflict

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,7 +7,7 @@ import logo from './logo/logo-black.png';
 import logotext from './logo/logo-text-black.png';
 
 import shaq from './personal/ak.png';
-import resume from 'resume.pdf';
+import resumeDoc from 'resume.pdf';
 
 import backend from './icons/backend.png';
 import ux from './icons/ux.png';
@@ -59,7 +59,7 @@ export {
   logo,
   logotext,
   shaq,
-  resume,
+  resumeDoc,
   backend,
   ux,
   frontend,
