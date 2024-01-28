5f22556175cf2cd2566c1d8f5e8bc1a8a787a7aa
anilk108 <43748737+anilk108@users.noreply.github.com>
2024-01-27 18:14:40 -0600

    contactform to API gateway

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,38 +25,35 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    // sign up on emailjs.com (select the gmail service and connect your account).
-    //click on create a new template then click on save.
-    emailjs
-      .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
-        {
-          from_name: form.name,
-          to_name: 'YourName', // put your name here.
-          from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
-          message: form.message,
-        },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert('Thank you. I will get back to you as soon as possible.');
+    // api gateway endpoint
+    fetch('https://icudjeo1d7.execute-api.us-east-1.amazonaws.com/portfolio-site/contactform', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Assuming your API returns JSON data
+    }).then(
+      () => {
+        setLoading(false);
+        alert('Thank you. I will get back to you as soon as possible.');
 
-          setForm({
-            name: '',
-            email: '',
-            message: '',
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.log(error);
-          alert('Something went wrong. Please try again.');
-        }
-      );
+        setForm({
+          name: '',
+          email: '',
+          message: '',
+        });
+      },
+      (error) => {
+        setLoading(false);
+        console.log(error);
+        alert('Something went wrong. Please try again.');
+      }
+    );
   };
 
   return (
